7dc59b69401d5a62749c15659b56ecbfbc9956fc
Rashaad Washington <washington_rashaad@yahoo.com>
2022-06-30 15:21:36 -0500

    Working on Fragments

--- app/src/main/java/com/biometradingcardcompany/biometradingcard/MainActivity.java
+++ app/src/main/java/com/biometradingcardcompany/biometradingcard/MainActivity.java
@@ -1,11 +1,11 @@
 package com.biometradingcardcompany.biometradingcard;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private TextView register;
